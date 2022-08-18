80d87a2c920a4cc0a681a1b875bc6640b8f2a8d2
GuiLeoni14 <guileone167@gmail.com>
2022-08-17 21:18:00 -0300

    adicionando styles da pagina home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import { SignButton } from '../SignButton';
-import styled from './styles.module.scss';
+import styles from './styles.module.scss';
 
 export function Header() {
     return (
-        <header className={styled.headerContainer}>
-            <div className={styled.headerContent}>
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a href="#" className={styled.active}>
+                    <a href="#" className={styles.active}>
                         home
                     </a>
                     <a href="#">posts</a>
