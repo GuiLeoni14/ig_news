7dfa815c3d68edc6993d62d04ab40b3aaae558d5
GuiLeoni14 <guileone167@gmail.com>
2022-08-18 19:31:27 -0300

    criando usuário no banco de dados, com base no email vindo do github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
     providers: [
@@ -10,4 +12,24 @@ export default NextAuth({
         }),
     ],
     secret: process.env.NEXT_AUTH_SECRET,
+    callbacks: {
+        async signIn({ user }) {
+            const { email } = user;
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(user.email as string)))),
+                        q.Create(q.Collection('users'), {
+                            data: { email },
+                        }),
+                        q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+                    ),
+                );
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        },
+    },
 });
