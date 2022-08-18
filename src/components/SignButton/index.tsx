d05476abaf9ffa21de167f6d3c6bc9f0c5f6c5ff
GuiLeoni14 <guileone167@gmail.com>
2022-08-18 18:44:12 -0300

    adicionando login via gihub

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -1,14 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export function SignButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
+    console.log(session);
+    const isUserLoggedIn = session;
+
     return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#04d361" /> Guilherme Leoni <FiX color="#737380" className={styles.closeIcon} />
+        <button type="button" className={styles.signInButton} onClick={() => signOut()}>
+            <FaGithub color="#04d361" /> {session.user?.name} <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" /> Sign in with Github
         </button>
     );
