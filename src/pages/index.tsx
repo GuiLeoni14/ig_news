0fdbbea515caf434d8b67d155a2d86ad408fc33d
GuiLeoni14 <guileone167@gmail.com>
2022-08-17 22:47:43 -0300

    adicionando static props na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+export type TPageHomeProps = {
+    product: {
+        priceId: string;
+        amount: string | null;
+    };
+};
+
+export default function PageHome({ product }: TPageHomeProps) {
     return (
         <>
             <Head>
@@ -16,12 +25,35 @@ export default function Home() {
                     </h1>
                     <p>
                         Get access to all publications <br />
-                        <span>for $9.99 mouth</span>
+                        <span>for {product.amount} mouth</span>
                     </p>
-                    <SubscribeButton />
+                    <SubscribeButton priceId={product.priceId} />
                 </section>
                 <img src="/images/avatar.svg" alt="girl coding" />
             </main>
         </>
     );
 }
+
+export const getStaticProps: GetStaticProps<TPageHomeProps> = async () => {
+    // retrieve apenas um elemento
+    // expand retorna todas as informações do produto por default retorna apenas o id do produto ligado ao preço
+    const price = await stripe.prices.retrieve('price_1LXwcyCWf3dovHWdvf6kvYWY', {
+        expand: ['product'],
+    });
+    const product = {
+        priceId: price.id,
+        amount: price.unit_amount
+            ? new Intl.NumberFormat('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+              }).format(price.unit_amount / 100) // product vem em centavos pro isso dividimos pro cem
+            : null,
+    };
+    return {
+        props: {
+            product,
+        },
+        revalidate: 60 * 60 * 24, // 24 hors 60s, 60min, 24hrs
+    };
+};
