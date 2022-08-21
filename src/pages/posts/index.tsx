e4055e8baa1a3e947f5fa903cec7f773867cad41
GuiLeoni14 <guileone167@gmail.com>
2022-08-21 15:22:49 -0300

    devolvendo os dados da paginas de posts para o client-side

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,19 @@ import { GetStaticProps, GetStaticPropsContext } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
 import styles from './styles.module.scss';
 
-export default function PagePosts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
+interface PagePostsProps {
+    posts: Post[];
+}
+export default function PagePosts({ posts }: PagePostsProps) {
     return (
         <>
             <Head>
@@ -13,56 +23,17 @@ export default function PagePosts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <Link href="#">
-                        <a>
-                            <time>12 de março de 2821</time>
-                            <strong>Titulo do posts</strong>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam explicabo
-                                obcaecati voluptatem consequatur, beatae cum consequuntur accusantium.
-                            </p>
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a>
-                            <time>12 de março de 2821</time>
-                            <strong>Titulo do posts</strong>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam explicabo
-                                obcaecati voluptatem consequatur, beatae cum consequuntur accusantium.
-                            </p>
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a>
-                            <time>12 de março de 2821</time>
-                            <strong>Titulo do posts</strong>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam explicabo
-                                obcaecati voluptatem consequatur, beatae cum consequuntur accusantium.
-                            </p>
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a>
-                            <time>12 de março de 2821</time>
-                            <strong>Titulo do posts</strong>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam explicabo
-                                obcaecati voluptatem consequatur, beatae cum consequuntur accusantium.
-                            </p>
-                        </a>
-                    </Link>
-                    <Link href="#">
-                        <a>
-                            <time>12 de março de 2821</time>
-                            <strong>Titulo do posts</strong>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam explicabo
-                                obcaecati voluptatem consequatur, beatae cum consequuntur accusantium.
-                            </p>
-                        </a>
-                    </Link>
+                    {posts.map((post) => {
+                        return (
+                            <Link href="#" key={post.slug}>
+                                <a>
+                                    <time>{post.updatedAt}</time>
+                                    <strong>{post.title}</strong>
+                                    <p>{post.excerpt}</p>
+                                </a>
+                            </Link>
+                        );
+                    })}
                 </div>
             </main>
         </>
@@ -77,8 +48,22 @@ export const getStaticProps: GetStaticProps = async (ctx: GetStaticPropsContext)
         fetch: ['publicar.title', 'publicar.content'],
         pageSize: 100,
     });
+
+    const posts = response.map((post) => {
+        // excerpt: percorrer todos os content e pegar o primeiro onde o tipo e paragrafo
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find((content: any) => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            }),
+        };
+    });
     console.log('response', JSON.stringify(response, null, 2));
     return {
-        props: {},
+        props: { posts },
     };
 };
